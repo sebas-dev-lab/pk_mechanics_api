1a421189f09a7c09fda5a453996a9523a8947d6c
Sebastian <lb.lescano.vs@gmail.com>
2022-05-24 06:13:34 -0300

    [Test]-Test application. Adding changelogs.

--- src/controllers/Authuser.controllers.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Extradata, ExtradataLogin } from '../config/interfaces/Auth.interfaces';
-import AuthServices from '../services/Auth.services';
-import Util from '../utils/Utils';
-
-export default class AuthUserControllers {
-  static async createnewuser(req: express.Request, res: express.Response) {
-  }
-}
