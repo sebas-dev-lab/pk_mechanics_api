2fd24c047a2f430a51a2a6bbc7a574fe3157f5a4
Sebastian Lescano <lescanovsl@gmail.com>
2022-10-26 23:22:26 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import Server from './config/server/server';
 import createDataToTest from './utils/CreateData.utils';
 
 const server = new Server();
+console.log('entro')
 
 server.start(createDataToTest);
